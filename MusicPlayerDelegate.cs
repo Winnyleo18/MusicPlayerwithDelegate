8c4fad822122cec83673a92123e4d6de86364a02
hp <hp@WINNYLEO18>
2024-03-18 22:33:27 +0100

    Add project files.

---
+++ MusicPlayerDelegate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Define a delegate for playlist events
+public delegate void PlaylistEventHandler(string message);
+
+public class MusicPlayerDelegate
+{
+    private List<string> playlist = new List<string>();
+
+    // Define events based on the delegate
+    public event PlaylistEventHandler SongAdded;
+    public event PlaylistEventHandler SongDeleted;
+
+    // Add a song to the playlist
+    public void AddSong(string song)
+    {
+        playlist.Add(song);
+        OnSongAdded(song);
+    }
+
+    // Delete a song from the playlist
+    public void DeleteSong(string song)
+    {
+        if (playlist.Remove(song))
+        {
+            OnSongDeleted(song);
+        }
+    }
+
+    // Play all songs in the playlist
+    public void PlayPlaylist()
+    {
+        Console.WriteLine("\nPlaying Playlist:");
+        foreach (string song in playlist)
+        {
+            Console.WriteLine($"Now playing: {song}");
+        }
+    }
+
+    // Shuffle the songs in the playlist
+    public void ShufflePlaylist()
+    {
+        Random random = new Random();
+        playlist = playlist.OrderBy(x => random.Next()).ToList();
+        Console.WriteLine("Playlist shuffled.");
+    }
+
+    // Sort the songs in the playlist
+    public void SortPlaylist()
+    {
+        playlist.Sort();
+        Console.WriteLine("Playlist sorted.");
+    }
+
+    // Display all songs in the playlist
+    public void DisplayPlaylist()
+    {
+        if (playlist.Count == 0)
+        {
+            Console.WriteLine("Playlist is empty.");
+        }
+        else
+        {
+            Console.WriteLine("\nCurrent Playlist:");
+            for (int i = 0; i < playlist.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {playlist[i]}");
+            }
+        }
+    }
+
+    // Raise the SongAdded event
+    protected virtual void OnSongAdded(string song)
+    {
+        SongAdded?.Invoke($"Song '{song}' added to the playlist.");
+    }
+
+    // Raise the SongDeleted event
+    protected virtual void OnSongDeleted(string song)
+    {
+        SongDeleted?.Invoke($"Song '{song}' deleted from the playlist.");
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MusicPlayer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of MusicPlayer
+            MusicPlayerDelegate musicPlayer = new MusicPlayerDelegate();
+
+            // Subscribe to events
+            musicPlayer.SongAdded += DisplayMessage;
+            musicPlayer.SongDeleted += DisplayMessage;
+
+            // Add songs to the playlist
+            musicPlayer.AddSong("Song 1");
+            musicPlayer.AddSong("Song 2");
+            musicPlayer.AddSong("Song 3");
+
+            // Display and play the playlist
+            musicPlayer.DisplayPlaylist();
+            musicPlayer.PlayPlaylist();
+
+            // Delete a song from the playlist
+            musicPlayer.DeleteSong("Song 2");
+
+            // Display and shuffle the playlist
+            musicPlayer.DisplayPlaylist();
+            musicPlayer.ShufflePlaylist();
+
+            // Display and sort the playlist
+            musicPlayer.DisplayPlaylist();
+            musicPlayer.SortPlaylist();
+        }
+
+        // Event handler method to display messages
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+}
+
+
+
+
+
+   
